b1e0acfed05d195f870bb560792a6834d9b9878f
intrnl <intrnl28+git@gmail.com>
2021-02-28 15:43:12 +0700

    chore: use temporal as example
    it's much easier to do it this way

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-let PST_OFFSET = 7 * 60;
+let { Temporal } = require('proposal-temporal');
 
-let MIN_IN_MS = 60000;
-let DAY_IN_MS = 86400000;
 
-function getPSTDate () {
-	let date = new Date();
-	let offset = date.getTimezoneOffset();
-
-	if (offset != PST_OFFSET) {
-		date = new Date(date.getTime() - (offset * MIN_IN_MS) + (PST_OFFSET * MIN_IN_MS));
-	}
-
-	return date;
-}
-
-function getDailySchedule (type, date = getPSTDate()) {
-	let now = date.getTime();
-
+function getDailySchedule (type, date = Temporal.now.zonedDateTimeISO('America/Los_Angeles')) {
 	let data = require(`./data/${type}.json`);
-	let start = new Date(data.start).getTime();
+	let start = Temporal.ZonedDateTime.from(data.start);
 	let cycle = data.cycle;
 
-	let day = (Math.abs(Math.round(start / DAY_IN_MS) - Math.round(now / DAY_IN_MS)) % cycle) + 1;
+	let diff = Math.abs(date.since(start, { largestUnit: 'day', smallestUnit: 'day' }).days);
+	let sched = (diff % cycle) + 1;
 
-	return data.items.filter((item) => item.schedule.includes(day));
+	return data.items.filter((item) => item.schedule.includes(sched));
 }
+
+console.log(getDailySchedule('quest-recommended'))
