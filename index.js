d26945be3dbcc3a41d28b0b8689fb8a40229c624
intrnl <intrnl28+git@gmail.com>
2021-02-28 21:39:14 +0700

    chore: actually make this work in PT
    this would've been a pain in the ass to figure out
    using the js date object
    
    thanks temporal

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let { Temporal } = require('proposal-temporal');
 
 
-function getDailySchedule (type, date = Temporal.now.zonedDateTimeISO('America/Los_Angeles')) {
+function getDailySchedule (type, date = Temporal.now.zonedDateTimeISO()) {
 	let data = require(`./data/${type}.json`);
+
 	let start = Temporal.ZonedDateTime.from(data.start);
+	let now = date.withTimeZone('America/Los_Angeles');
+
 	let cycle = data.cycle;
 
-	let diff = Math.abs(date.since(start, { largestUnit: 'day', smallestUnit: 'day' }).days);
-	let sched = (diff % cycle) + 1;
+	let diff = now.since(start, { largestUnit: 'day', smallestUnit: 'day' }).days;
+	let day = (Math.abs(diff) % cycle) + 1;
 
-	return data.items.filter((item) => item.schedule.includes(sched));
+	return data.items.filter((item) => item.schedule.includes(day));
 }
 
 console.log(getDailySchedule('quest-recommended'))
